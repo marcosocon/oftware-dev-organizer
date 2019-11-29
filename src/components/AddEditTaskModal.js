b5a197067006be4eae110f7d2e74c5d55208c3be
marcosocon <oconmarcos97@gmail.com>
2019-11-28 21:48:23 -0300

    added date picker and raw form for add/edit tasks

--- src/components/AddEditTaskModal.js
+++ src/components/AddEditTaskModal.js
@@ -1,17 +1,76 @@
 import React from 'react';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
+import MomentUtils from '@date-io/moment';
+import {
+	Dialog,
+	DialogTitle,
+	DialogActions,
+	Button,
+	TextField,
+	Select,
+	MenuItem
+} from '@material-ui/core';
+import {
+	MuiPickersUtilsProvider,
+	KeyboardDatePicker,
+  } from '@material-ui/pickers';
+
+import {MyContext} from './../Context';
+import moment from 'moment';
 
 class AddEditTaskModal extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			taskName: '',
+			taskProjectId: '',
+			dueDate: moment()
+		}
+	}
 	handleClose = () => {
 		this.props.onClose();
 	};
 
 	render() {
 		return (
-			<Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.props.open}>
-				<DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
-			</Dialog>
+			<MyContext.Consumer>
+				{(context) => {
+					const { projects } = context.state;
+					return (
+						<Dialog
+							onClose={this.handleClose}
+							open={this.props.open}>
+							<DialogTitle>Add / Edit Task</DialogTitle>
+							<TextField
+								label="Task Name"
+								onChange={(e) => this.setState({taskName: e.target.value})}
+								value={this.state.taskName}/>
+							<Select
+								value={this.state.taskProjectId}
+								onChange={(e) => this.setState({taskProjectId: e.target.value})}>
+								{projects.map((proj) => <MenuItem value={proj.id}>{proj.name}</MenuItem>)}
+							</Select>
+							<MuiPickersUtilsProvider utils={MomentUtils}>
+								<KeyboardDatePicker
+									format="DD/MM/YYYY"
+									value={this.state.dueDate}
+									onChange={(date => this.setState({dueDate: moment(date)}))}	
+									label="Dues on"/>
+							</MuiPickersUtilsProvider>
+							<DialogActions>
+								<Button color="default">
+									Cancel
+								</Button>
+								<Button
+									onClick={()=>console.log(this.state)}
+									autoFocus>
+									Save
+								</Button>
+							</DialogActions>
+						</Dialog>
+					)
+				}}
+			</MyContext.Consumer>
 		)
 	}
 }
